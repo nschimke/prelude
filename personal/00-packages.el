9ef73599e8bff1e072e4dbbfbfd50c43b49a1578
Nathan Schimke <nathan.schimke@lacework.net>
2023-09-13 17:02:32 -0700

    Add terraform mode

--- personal/00-packages.el
+++ personal/00-packages.el
@@ -1,1 +1,3 @@
-(prelude-require-packages '(org-journal org-journal-tags highlight-indent-guides monokai-theme))
+(prelude-require-packages '(org-journal org-journal-tags
+                                        highlight-indent-guides monokai-theme
+                                        terraform-mode terraform-doc))
